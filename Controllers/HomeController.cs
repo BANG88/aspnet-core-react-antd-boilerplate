8f3d148e13dff3a65a679958f66161875c62a5bb
bang <sqibang@gmail.com>
2017-04-02 02:32:31 +0700

    refactor: rename to ant

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace WebApplicationBasic.Controllers
+namespace Ant.Controllers
 {
     public class HomeController : Controller
     {
